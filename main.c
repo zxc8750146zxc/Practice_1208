d0c653b94f3774544bbf21b6cc07bc44b82ced9f
zxc8750146zxc <115057656+zxc8750146zxc@users.noreply.github.com>
2022-12-08 23:31:28 +0800

    Add files via upload
    it is odd number of rang(1000 ~ 1500) .

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(void) {
+	
+int number = 999;
+
+	for (number = 1000; number < 1500; number++){
+		 	if (number%2!=0)
+		 		
+		 		printf("%d\n",number);
+		 }
+		 
+	
+	
+	
+	
+	
+	return 0;
+}
